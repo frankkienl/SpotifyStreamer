abdf918338d441a91a4eef1527f06c997af8fe7d
Frank Bouwens <frankkie12345@gmail.com>
2015-06-13 12:43:06 +0200

    initial work on ListAdapter

--- app/src/main/java/nl/frankkie/spotifystreamer/SearchArtistAdapter.java
+++ app/src/main/java/nl/frankkie/spotifystreamer/SearchArtistAdapter.java
@@ -1,7 +1,30 @@
 package nl.frankkie.spotifystreamer;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
 /**
  * Created by FrankkieNL on 9-6-2015.
  */
-public class SearchArtistAdapter {
+public class SearchArtistAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
